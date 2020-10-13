80be20c719ce920eb5bcae0f57ccf086f61a2f63
Aaron Rotman <58377413+aaronrotman@users.noreply.github.com>
2020-10-12 21:39:58 -0700

    Add commands to get Ethereum gas and price data

--- discord_bot.py
+++ discord_bot.py
@@ -1,6 +1,7 @@
 # Dependencies
 import discord
 from config import discord_token
+from functions import get_gas_data, get_eth_price
 
 # Instantiate Client to connect to discord
 client = discord.Client()
@@ -17,12 +18,23 @@ async def on_message(message):
     # Ignore messages sent by this bot
     if message.author == client.user:
         return
-    # Respond to messages that start with '$hello '
+    # $hello | Respond with 'Hello!'
     if message.content.lower().startswith('$hello'):
         await message.channel.send('Hello!')
-    # Test response
-    elif message.content.lower().startswith('$test'):
-        await message.channel.send("Testing...testing...1...2...3")
+    
+    # $gas | Respond with current Ethereum gas price
+    elif message.content.lower().startswith('$gas'):
+        # Make API call to get eth gas data
+        gas_data = get_gas_data()
+        # Send the results to the discord channel
+        await message.channel.send(f"Safe: {gas_data['safe_gas']}\nPropose: {gas_data['propose_gas']}")
+   
+    # $eth | Respond with current Ethereum price
+    elif message.content.lower().startswith("$eth"):
+        # Make API call to get eth price data
+        eth_data = get_eth_price()
+        # Send the results to the discord channel
+        await message.channel.send(f"Ethereum: ${eth_data}")
 
 # Run the app
 client.run(discord_token)
---
+++ functions.py
@@ -0,0 +1,57 @@
+# Dependencies
+from config import etherscan_key
+import requests
+import json
+
+
+# Function to return current Ethereum gas data
+def get_gas_data():
+    try:
+        query_url = f"https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey={etherscan_key}"
+        gas_response = requests.get(query_url)
+        gas_price = json.loads(gas_response.text)
+        results = gas_price['result']
+        gas_dict = {
+            "last_block": results['LastBlock'],
+            "safe_gas": f"{results['SafeGasPrice']} gwei",
+            "propose_gas": f"{results['ProposeGasPrice']} gwei"
+        }
+        return gas_dict
+
+    # Handle api call failures
+    except Exception as e:
+        print(e)
+        gas_dict = {
+            "last_block": "Data Unavailable",
+            "safe_gas": "Data Unavailable",
+            "propose_gas": "Data Unavailable"
+        }
+        return gas_dict
+
+# Function to return the current Ethereum price data
+def get_eth_price():
+    try:
+        eth_price_url = f"https://api.etherscan.io/api?module=stats&action=ethprice&apikey={etherscan_key}"
+        eth_response = requests.get(eth_price_url)
+        eth_data = json.loads(eth_response.text)
+        eth_result = eth_data['result']
+        eth_price = eth_result['ethusd']
+        # Convert timestamp into a string
+        # epoch_time = int(eth_result['ethusd_timestamp'])
+        # string_time = datetime.fromtimestamp(epoch_time).strftime('%X')
+
+        # eth_dict = {
+        #     "eth_usd": eth_result['ethusd'],
+        #     "time": string_time
+        # }
+        return eth_price
+
+    # Handle api call failures
+    except Exception as e:
+        print(e)
+        eth_price = "Data Unavailable"
+        # eth_dict = {
+        #     "eth_usd": "Data Unavailable",
+        #     "time": "Data Unavailable"
+        # }
+        return eth_price
